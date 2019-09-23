1368d15f9358612ce2f5a18285ff47aaaa4fcd78
KUASWoodyLin <jj7158761@gmail.com>
2019-09-24 02:51:03 +0800

    fix detection_test.py

--- detection_test.py
+++ detection_test.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from detection import parse_aug_fn
 
 # Read Image
-img_string = tf.io.read_file('test/test_detection_image.png')
+img_string = tf.io.read_file('test/test_detection_image.jpg')
 img = tf.image.decode_image(img_string)
 img = img.numpy()
 
@@ -16,23 +16,24 @@ with open('test/test_detection_label.txt', 'r') as file:
     bboxes, labels = [], []
     for line in lines:
         bboxes.append([float(box) for box in line.split()[:-1]])
-        labels.append(line.split()[-1])
+        labels.append(int(line.split()[-1]))
 classes_list = ['bottle', 'tvmonitor', 'person']
 
 # Draw Image and Bounding bix
-h, w, _ = img.shape
+draw_img = img.copy()
+h, w, _ = draw_img.shape
 for bbox, label in zip(bboxes, labels):
     y1 = int(bbox[0] * h)
     x1 = int(bbox[1] * w)
     y2 = int(bbox[2] * h)
     x2 = int(bbox[3] * w)
-    cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-    cv2.putText(img,
-                label,
+    cv2.rectangle(draw_img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    cv2.putText(draw_img,
+                classes_list[label],
                 (x1, y1 - 3),
                 cv2.FONT_HERSHEY_SIMPLEX,
                 1, (0, 244, 0), 2)
-plt.imshow(img)
+plt.imshow(draw_img)
 
 
 # Create dataset pipline
@@ -45,7 +46,7 @@ img_objects_ds = img_objects_ds.map(lambda dataset: parse_aug_fn(dataset, (416, 
                                     num_parallel_calls=tf.data.experimental.AUTOTUNE)
 h, w = (416, 416)
 images = np.zeros((h * 4, w * 4, 3))
-for count, [img, bboxes, label] in enumerate(img_objects_ds.repeat().take(16)):
+for count, [img, bboxes] in enumerate(img_objects_ds.repeat().take(16)):
     bboxes = tf.multiply(bboxes, [h, w, h, w, 1])
     img = img.numpy()
     box_indices = tf.where(tf.reduce_sum(bboxes, axis=-1))
@@ -67,3 +68,4 @@ for count, [img, bboxes, label] in enumerate(img_objects_ds.repeat().take(16)):
     images[h * i:h * (i + 1), w * j:w * (j + 1)] = img
 plt.figure(figsize=(12, 12))
 plt.imshow(images)
+plt.show()
